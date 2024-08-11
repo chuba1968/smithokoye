84b458e39d2e0329f8f968000543b119f216d79d
chuba1968 <63740611+chuba1968@users.noreply.github.com>
2024-08-11 13:35:27 +0100

    Initial commit

---
+++ android/app/src/main/java/io/smithokoye/nba/MainActivity.java
@@ -0,0 +1,5 @@
+package io.smithokoye.nba;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,29 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+/// <reference types="@capacitor/splash-screen" />
+
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Smith and okoye\\',
-  webDir: 'www'
+  appId: 'io.smithokoye.nba',
+  appName: 'Smith and okoye',
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
